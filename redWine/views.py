8cd3654afd09cdd9fc2861df571e68c9b77a93bb
nixo <nixatto@gmail.com>
2013-11-05 20:41:31 +0100

    Initial files to test stuff and get started!

---
+++ redWine/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Context
+import datetime
+
+def hello(request):
+    return HttpResponse("Hello world")
+
+def dateTime(request):
+    now=datetime.datetime.now()
+    html="<html><body>The Time Is: %s.</body></html>" % now
+    return HttpResponse(html)
+
+def hours_ahead(request, offset):
+    try:
+        offset=int(offset)
+    except:
+        raise Http404()
+    dt=datetime.datetime.now() + datetime.timedelta(hours=offset)
+    t = get_template('date.html')
+    html = t.render(Context({'current_date': dt}))
+    return HttpResponse(html)
