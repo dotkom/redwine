77c6b26698bb5d4bfcd983daa04573235e7630dd
nixo <nixatto@gmail.com>
2013-11-26 22:45:52 +0100

    Fixed static files

--- redWine/urls.py
+++ redWine/urls.py
@@ -5,11 +5,4 @@ from redWine.views import home
 
 urlpatterns = patterns('',
     url (r'^$', home),
-    # Examples:
-    # url(r'^$', 'vinstraff.views.home', name='home'),
-    # url(r'^vinstraff/', include('vinstraff.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-     #url(r'^admin/', include(admin.site.urls)),
 )
