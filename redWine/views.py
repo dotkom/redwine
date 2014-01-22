3baba88a004f0298194e3363def892afcc35ef1a
nixo <nixatto@gmail.com>
2014-01-22 21:01:58 +0100

    Changed static strings with variables

--- redWine/views.py
+++ redWine/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.http import HttpResponse
 from django.template.loader import get_template
 from django.template import Context
@@ -5,4 +6,15 @@ from django.shortcuts import render, get_object_or_404, redirect
 import datetime
 
 def home(request):
-    return render(request, 'index.html', {'test' : 1})
+    wines=6
+    return render(request, 'index.html', {  
+        'username' : 'Nicolas Tester',
+        'wines' : wines,
+        'wineList' : range(3),
+        'wineString' : str(wines),
+        'isAdmin' : True,
+        'name' : 'Nicolas Mifadutti Poppy',
+        'time' : '2014-01-15 13:37',
+        'author' : 'Nils',
+        'reason' : 'Spiste for mye pizza og sendte ikke nok pupper på snap.'
+        })
