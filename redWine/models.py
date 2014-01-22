d956cb3c9800f4ef6fe1ad9092038a753d24c753
nixo <nixatto@gmail.com>
2014-01-22 23:02:28 +0100

    fixed models a bit more

--- redWine/models.py
+++ redWine/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 class Bruker(models.Model):
     name = models.CharField(max_length=30)
-    admin = models.CharField(max_length=50)
-    totalWines = models.CharField(max_length=60)
+    admin = models.BooleanField()
+    totalWines = models.PositiveIntegerField()
+    registered = models.DateTimeField()
+    komite = models.CharField(max_length=30)
 
 class Straff(models.Model):
     to = models.CharField(max_length=30)
-    giver = models.CharField(max_length=50)
-    amount = models.CharField(max_length=60)
-    reason = models.CharField(max_length=30)
-    date = models.DateField()
+    giver = models.CharField(max_length=30)
+    amount = models.PositiveIntegerField()
+    reason = models.CharField(max_length=80)
+    date = models.DateTimeField(auto_now=True)
