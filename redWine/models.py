27ce7da9173c68bcc1b41e027ef4d4e3f7530980
Nixo <me@nixo.no>
2014-03-05 21:30:26 +0000

    AAAAAAAAAAAlot of things, seeing the end

--- redWine/models.py
+++ redWine/models.py
@@ -7,7 +7,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User)  
     komite = models.CharField(max_length=30, default='dotKom')
     def total(self):
-        return sum([straff.amount for straff in self.straffer.all()])
+        return sum([straff.amount for straff in self.straffer.filter(deleted=False)])
     def __unicode__(self):
         return u'%s' % (self.user)
 
@@ -23,8 +23,11 @@ class Straff(models.Model):
     amount = models.PositiveIntegerField()
     reason = models.CharField(max_length=100)
     date = models.DateTimeField(auto_now=True)
+    deleted = models.BooleanField(default=False)
     def __unicode__(self):
         return u'%s - %s' % (self.amount, self.to)
     class Meta:
         ordering = ['-date']
         get_latest_by = 'date'
+
+
--- redWine/views.py
+++ redWine/views.py
@@ -13,6 +13,8 @@ from .forms import newPenaltyForm
 @login_required
 def home(request):
     submitted=False
+    showDeleted=False
+    editedUser=-1
     if request.method == 'POST':
         
         act=str(request.POST['act'])
@@ -28,20 +30,37 @@ def home(request):
                     reason=str(form.data['reason'].encode('utf8'))
                     )
             s.save()
+            editedUser=int(form.data['to'])-1
           else:
             raise PermissionDenied
             
-        elif 'delete' in act: #todo: sjekk at man ikke sletter sin egen
+        elif 'delete' in act: 
           straffen=Straff.objects.get(pk=int(act.split(" ")[1]))
           if str(straffen.to.user.username) != str(request.user):
-            straffen.delete()
+            straffen.deleted=True
+            straffen.save()
+            editedUser=straffen.to.user.id
+            
+        elif 'nuke' in act:
+          for straffen in Straff.objects.filter(to=UserProfile.objects.get(pk=int(act.split(" ")[1])-1)):
+            if str(straffen.to.user.username) != str(request.user):
+              straffen.deleted=True
+              straffen.save()
+              editedUser=int(act.split(" ")[1])-1
+        
+        elif 'showhidden' in act:
+          showDeleted=True
+          editedUser=int(act.split(" ")[1])-2
+        
     else:
         form=newPenaltyForm()
 
-    straffer=Straff.objects.all()
+    #straffer=Straff.objects.all()
     users=sorted(UserProfile.objects.all(), key=lambda a: a.total(), reverse=True)
     return render(request, 'index.html', {  
         'users' : users,
         'submittedNew' : submitted,
+        'showDeleted' : showDeleted,
+        'editedUser' : editedUser,
         #'form' : form,
         })
