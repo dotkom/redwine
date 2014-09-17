82331e1aa85132dc45e82232dab304904dc58c74
Nicolas A. Tonne <nixatto@gmail.com>
2014-09-17 22:48:37 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='redwine',
-    version='0.5',
+    version='0.55',
     packages=['redwine'],
     include_package_data=True,
     license='BSD License',  # example license
