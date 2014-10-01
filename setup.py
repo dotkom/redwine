a9cc99f70631701bd5b7700234fdefe5bc9ef69d
OptimusCrime <thomasgautv@hotmail.com>
2014-10-01 21:15:11 +0200

    Reworked setup.py to make stuff work again

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 import os
 from setuptools import setup
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
-
-# allow setup.py to be run from any path
+# Allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='redwine',
-    version='1.0.4',
+    version='1.0.5',
     packages=['redwine'],
     include_package_data=True,
     license='BSD License',  # example license
     description='A wine punishment management system.',
-    long_description=README,
     url='http://online.ntnu.no/redwine',
     author='dotKom',
     author_email='dotkom@online.ntnu.no',
