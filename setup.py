05c6301bca635cba5aee6cb71c2e0edc2ba7a26d
Nicolas A. Tonne <nixatto@gmail.com>
2014-10-08 21:09:26 +0200

    Removed readme from setup due to crashes

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 import os
 from setuptools import setup
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
-
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
@@ -13,7 +11,6 @@ setup(
     include_package_data=True,
     license='BSD License',  # example license
     description='A wine punishment management system.',
-    long_description=README,
     url='http://online.ntnu.no/redwine',
     author='dotKom',
     author_email='dotkom@online.ntnu.no',
