52de40938438ee5a1bfacfcdf601de7464884024
Nicolas A. Tonne <nixatto@gmail.com>
2014-10-15 23:52:56 +0200

    Updated to django version 1.7

--- redwine/models.py
+++ redwine/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.db.models import Count
 from django.conf import settings
 from django.db.models.signals import post_save
-from django.contrib.auth import get_user_model
+#from django.contrib.auth import get_user_model
 
 class Penalty(models.Model):
     
-    to =        models.ForeignKey(get_user_model(), related_name='penalties')
-    giver =     models.ForeignKey(get_user_model(), related_name='penaltygiver')
+    to =        models.ForeignKey(settings.AUTH_USER_MODEL, related_name='penalties')
+    giver =     models.ForeignKey(settings.AUTH_USER_MODEL, related_name='penaltygiver')
     amount =    models.PositiveIntegerField()
     committee = models.CharField(max_length=60)
     reason =    models.CharField(max_length=100)
